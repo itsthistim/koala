a67980e8fc99220531109c061a09152882f90156
Xavons <37299554+Xavons@users.noreply.github.com>
2021-01-06 02:54:14 +0100

    basic fix for discrim cmd

--- src/commands/info/discrim.js
+++ src/commands/info/discrim.js
@@ -21,7 +21,7 @@ module.exports = class DiscrimCommand extends Command {
     }
 
     *args() {
-        const discrim = yield {
+        const discriminator = yield {
             type: 'string',
             match: 'phrase',
             prompt: {
@@ -36,9 +36,28 @@ module.exports = class DiscrimCommand extends Command {
 
     async exec(message, { discriminator }) {
         
-        const embed = this.client.util.embed()
-            .setTitle(`${users.length} Users with the discriminator: ${discrim}`)
-            .setDescription(users.join(', '));
-        message.channel.send(embed);
+        let users = this.client.users.cache;
+        let matches = [];
+
+        users.forEach(element => {
+            if(element.discriminator === discriminator) {
+                matches.push(`${element.username}#${element.discriminator}`);
+            }
+        });
+
+        if (matches.length === 0) {
+            message.channel.send("No matches found.");
+        } else {
+            let reply = "```" + matches.join("\n") + "```";
+            reply += " Sample size: " + users.size;
+
+            message.channel.send(reply); // send user list 
+        }
+
+
+        // const embed = this.client.util.embed()
+        //     .setTitle(`${users.length} users with the discriminator: ${discriminator}`)
+        //     .setDescription(users.join(', '));
+        // message.channel.send(embed);
     }
 }
