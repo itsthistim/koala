91da6dc56bac286ec1322e865bc9ad4876fe7c9b
Xavons <37299554+Xavons@users.noreply.github.com>
2021-01-06 14:40:19 +0100

    added monke, snippets and cooldown delete

---
+++ src/commands/image/monke.js
@@ -0,0 +1,26 @@
+const { Command } = require('discord-akairo');
+const Logger = require('../../util/logger.js');
+
+module.exports = class MonkeCommand extends Command {
+    constructor() {
+        super('monke', {
+            aliases: ['monke'],
+            category: 'Image',
+            userPermissions: [],
+            clientPermissions: ['SEND_MESSAGES', 'EMBED_LINKS', 'ATTACH_FILES'],
+            ignorePermissions: [],
+            cooldown: 1,
+            ratelimit: 1,
+            ignoreCooldown: [],
+            ownerOnly: false,
+            description: {
+                content: 'Sends monke.',
+                usage: ''
+            },
+        })
+    }
+
+    async exec(msg, args) {
+        msg.util.send('https://cdn.discordapp.com/attachments/502208815937224718/796371361492107285/fcfd49e044a51e6f4c76a6748b8fc2fc.png')
+    }
+}
--- src/listeners/cooldown.js
+++ src/listeners/cooldown.js
@@ -10,17 +10,14 @@ module.exports = class CooldownListener extends Listener {
 		});
 	}
 
-	exec(message, command, remaining) {
+	async exec(message, command, remaining) {
 		const time = remaining / 1000;
 		const tag = message.guild ? message.guild.name : `${message.author.tag}/PM`;
 		Logger.log(`=> ${command.id} ~ ${time}`, { tag });
 
-		if (command.id == 'invis-detectorrrr') {
-			return;
-		}
-
 		if (message.guild ? message.channel.permissionsFor(this.client.user).has('SEND_MESSAGES') : true) {
-			message.reply(`you can use that command again in ${time} seconds.`);
+			let msg = await message.reply(`you can use that command again in ${time} seconds.`);
+			msg.delete({timeout: remaining});
 		}
 	}
 }
