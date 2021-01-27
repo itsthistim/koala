920834129a0f0d2f537ab91d04984edab83d581d
Xavons <37299554+Xavons@users.noreply.github.com>
2021-01-27 23:32:21 +0100

    added rainbow

--- src/commands/image/rainbow.js
+++ src/commands/image/rainbow.js
@@ -4,10 +4,10 @@ const axios = require('axios');
 const { createCanvas, loadImage } = require('canvas');
 const path = require('path');
 
-module.exports = class HandsCommand extends Command {
+module.exports = class RainbowCommand extends Command {
     constructor() {
-        super('hands', {
-            aliases: ['hands', 'hand'],
+        super('rainbow', {
+            aliases: ['rainbow', 'gay'],
             category: 'Image',
             userPermissions: [],
             clientPermissions: [],
@@ -16,7 +16,7 @@ module.exports = class HandsCommand extends Command {
             ratelimit: 1,
             ownerOnly: false,
             description: {
-                content: 'Will make a user raise their hands in a threatening way.',
+                content: 'Will show a members profile picture behind a rainbow.',
                 usage: '<member>'
             },
         })
@@ -39,18 +39,16 @@ module.exports = class HandsCommand extends Command {
 
     async exec(msg, { u }) {    
         try {
-            const base = await loadImage("https://cdn.discordapp.com/attachments/502208815937224718/804113450481352704/hands.png");
+            const base = await loadImage("https://media.discordapp.net/attachments/502208815937224718/804116108365529108/rainbow.png");
             const data = await loadImage(u.user.avatarURL({format: 'png'}));
-            
+
 			const canvas = createCanvas(data.width, data.height);
 			const ctx = canvas.getContext('2d');
 			ctx.drawImage(data, 0, 0);
-			const ratio = data.width / base.width;
-			const height = base.height * ratio;
-			ctx.drawImage(base, 0, data.height - height, data.width, height);
+			ctx.drawImage(base, 0, 0, data.width, data.height);
 			const attachment = canvas.toBuffer();
 			if (Buffer.byteLength(attachment) > 8e+6) return msg.util.send('Resulting image was above 8 MB.');
-			return msg.util.send({ files: [{ attachment, name: 'hands.png' }] });
+			return msg.util.send({ files: [{ attachment, name: 'rainbow.png' }] });
 		} catch (err) {
 			return msg.util.send(`Oh no, an error occurred: \`${err.message}\`. Try again later!`);
 		}
