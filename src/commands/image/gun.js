e8505c352e5a8b19d62635d2b0283d8625d51835
Xavons <37299554+Xavons@users.noreply.github.com>
2021-01-27 23:23:02 +0100

    small msg fix

--- src/commands/image/gun.js
+++ src/commands/image/gun.js
@@ -43,18 +43,12 @@ module.exports = class GunCommand extends Command {
             const data = await loadImage(u.user.avatarURL({format: 'png'}));
             const canvas = createCanvas(data.width, data.height);
             const ctx = canvas.getContext('2d');
-            
             ctx.drawImage(data, 0, 0);
-            
             const ratio = (data.height / 2) / base.height;
             const width = base.width * ratio;
-            
             ctx.drawImage(base, data.width - width, data.height - (data.height / 2), width, data.height / 2);
-            
             const attachment = canvas.toBuffer();
-            
-            if (Buffer.byteLength(attachment) > 8e+6) return msg.reply('Resulting image was above 8 MB.');
-            
+            if (Buffer.byteLength(attachment) > 8e+6) return msg.util.send('Resulting image was above 8 MB.');
             return msg.util.send({ files: [{ attachment, name: 'gun.png' }] });
         } catch (err) {
             return msg.util.send(`Oh no, an error occurred: \`${err.message}\`. Try again later!`);
