7308f88cbdb6f12417c3a97622b423954268edea
Xavons <37299554+Xavons@users.noreply.github.com>
2021-03-15 16:18:54 +0100

    fixed typo

--- src/commands/image/ghandi-quote.js
+++ src/commands/image/ghandi-quote.js
@@ -7,7 +7,7 @@ const path = require('path');
 module.exports = class GhandiQuoteCommand extends Command {
     constructor() {
         super('gandhi-quote', {
-            aliases: ['ghandi-quote', 'ghandi', 'gandhi'],
+            aliases: ['gandhi-quote', 'ghandi-quote', 'ghandi', 'gandhi'],
             category: 'Image',
             userPermissions: [],
             clientPermissions: ['ATTACH_FILES'],
