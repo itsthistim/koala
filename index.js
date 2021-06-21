6493ec3698e97b0722eaa95853528a638f856317
Xavons <37299554+Xavons@users.noreply.github.com>
2021-06-21 22:27:21 +0200

    first attempt making a color type

--- index.js
+++ index.js
@@ -112,18 +112,24 @@ class Client extends AkairoClient {
 
 	//#region Custom Types
 
+		this.commandHandler.resolver.addType('color', async (message, phrase) => {
+			if (!phrase) return null;
+                const color = phrase.startsWith('[') ? phrase.slice(phrase.indexOf('[') + 1).slice(0, phrase.indexOf(']') - 1).split(',').map(e => parseInt(e.trim())) : phrase.toUpperCase().replace(' ', '_');
+                return this.client.util.resolveColor(color);
+		});
+
 		this.commandHandler.resolver.addType('timespan', async (message, phrase) => {
 			if (!phrase) return null;
 
 			var TimeUnits = {
 				years: { label: '(?:years?|y)', value: 1000 * 60 * 60 * 24 * 365 },
-				months: { label: '(?:months?|mo)', value: 1000 * 60 * 60 * 24 * 30 },
+				months: { label: '(?:months?|mons?|mo)', value: 1000 * 60 * 60 * 24 * 30 },
 				weeks: { label: '(?:weeks?|w)', value: 1000 * 60 * 60 * 24 * 7 },
 				days: { label: '(?:days?|d)', value: 1000 * 60 * 60 * 24 },
-				hours: { label: '(?:hours?|hrs?|h)', value: 1000 * 60 * 60 },
+				hours: { label: '(?:hours?|hrs?|hos?|h)', value: 1000 * 60 * 60 },
 				minutes: { label: '(?:minutes?|mins?|m)', value: 1000 * 60 },
 				seconds: { label: '(?:seconds?|secs?|s)', value: 1000 },
-				milliseconds: { label: '(?:milliseconds?|msecs?|ms)', value: 1 }
+				milliseconds: { label: '(?:milliseconds?|msecs?|mills?|millis?|ms)', value: 1 }
 			}
 
             const regexString = Object.entries(TimeUnits).map(([name, { label }]) => String.raw`(?:(?<${name}>-?(?:\d+)?\.?\d+) *${label})?`).join('\\s*');
