3a5e3232074d5969b6bb7d0b17bd863a7103fce1
Xavons <37299554+Xavons@users.noreply.github.com>
2021-07-13 21:46:12 +0200

    cleanup unscramble

--- src/commands/Utility/unscramble.js
+++ src/commands/Utility/unscramble.js
@@ -34,7 +34,6 @@ module.exports = class UnscrambleCommand extends Command {
     }
 
     async exec(msg, args) {
-        
         if (!args.word) {
             return msg.channel.send('Wrong args sth is still undefined')
         }
