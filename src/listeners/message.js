de7962884b902221e677a52f9d29d60429c0fc6a
Xavons <37299554+Xavons@users.noreply.github.com>
2021-09-01 12:12:23 +0200

    cleaned up message listener

--- src/listeners/message.js
+++ src/listeners/message.js
@@ -13,111 +13,31 @@ class MessageListener extends Listener {
 	}
 
 	async exec(msg) {
-
-	//#region Resurgence Server
-
-	let hamWords = ['ham', 'hamborger', 'hamborgerx', '616801307629453314'];
-			let thorWords = ['thorminator', 'dad', 'daddy', '329796755665190914'];
-			let xirWords = ['xir', 'xiribidus', '729374984551792651'];
+		//#region Resurgence Server
+		if ((msg.guild == '502208815937224715' /*..*/ || msg.guild == '752929101412827286' /*Resurgence*/) && msg.channel.type == "text") {
+			let xavWords = ['xav', 'xa', 'xavons', 'x', '319183644331606016'];
+			let hamWords = ['ham', 'hamborger', 'hamborgerx', '616801307629453314'];
+			let thorWords = ['thor', 'thorminator', 'dad', 'daddy', '329796755665190914'];
+			let xirWords = ['xir', 'xiri', 'xiribidus', '729374984551792651'];
 			let walWords = ['wal', 'walrus', 'Walrus5', '467353225104850954'];
-			let xavWords = ['x', 'xa', 'xav', 'xavons', '319183644331606016'];
 			let agaWords = ['aga', 'agamemnon', 'agame', 'agamne', '346464240703504385'];
 			let dragWords = ['drag', 'dragon', 'dragonblazer', 'dragonblazer100', '722198795844059168'];
 			let yonnorWords = ['yon', 'yonnor', 'yonnorc', 'yonnorc42', 'yonnor42', '281648176434511873'];
 			let mightyWords = ['mighty', 'connor', 'connormurphy', 'murphy', '291385157401706496'];
 
-			if (msg.channel.type == "text") {
-				if (msg.guild.id == '752929101412827286') {
-					const foundThor = thorWords.find((word) => {
-						const regex = new RegExp(`\\b${word}\\b`, 'i');
-						return regex.test(msg.content);
-					});
+			findWords(xavWords, msg);
+			findWords(hamWords, msg);
+			findWords(thorWords, msg);
+			findWords(xirWords, msg);
+			findWords(walWords, msg);
+			findWords(agaWords, msg);
+			findWords(dragWords, msg);
+			findWords(yonnorWords, msg);
+			findWords(mightyWords, msg);
+		}
+		//#endregion
 
-					const foundXav = xavWords.find((word) => {
-						const regex = new RegExp(`\\b${word}\\b`, 'i');
-						return regex.test(msg.content);
-					});
-
-					const foundDrag = dragWords.find((word) => {
-						const regex = new RegExp(`\\b${word}\\b`, 'i');
-						return regex.test(msg.content);
-					});
-
-					const foundWal = walWords.find((word) => {
-						const regex = new RegExp(`\\b${word}\\b`, 'i');
-						return regex.test(msg.content);
-					});
-
-					const foundAga = agaWords.find((word) => {
-						const regex = new RegExp(`\\b${word}\\b`, 'i');
-						return regex.test(msg.content);
-					});
-
-					const foundYonnor = yonnorWords.find((word) => {
-						const regex = new RegExp(`\\b${word}\\b`, 'i');
-						return regex.test(msg.content);
-					});
-
-					const foundXir = xirWords.find((word) => {
-						const regex = new RegExp(`\\b${word}\\b`, 'i');
-						return regex.test(msg.content);
-					});
-
-					const foundHam = hamWords.find((word) => {
-						const regex = new RegExp(`\\b${word}\\b`, 'i');
-						return regex.test(msg.content);
-					});
-
-					const foundMighty = mightyWords.find((word) => {
-						const regex = new RegExp(`\\b${word}\\b`, 'i');
-						return regex.test(msg.content);
-					});
-
-					if (foundXav) {
-						msg.react('717915043852845157');
-					}
-
-					if (foundThor) {
-						//msg.react('779704880792469514'); // purple
-						//msg.react('779745006944256010'); // corn
-						msg.react('782662912225443841'); // irl pfp
-					}
-
-					if (foundWal) {
-						//msg.react('779720093608837130'); // wal default
-						msg.react('786654445013827616'); // female wal
-					}
-
-					if (foundDrag) {
-						msg.react('779736525533216829');
-					}
-
-					if (foundXir) {
-						msg.react('764192509773479977');
-					}
-
-					if (foundAga) {
-						msg.react('779730263524900875');
-					}
-
-					if (foundHam) {
-						//msg.react('776204703259951134'); // hamburger
-						msg.react('784122296784257075'); // dark monster thing
-					}
-
-					if (foundYonnor) {
-						msg.react('781222395603451914');
-					}
-
-					if (foundMighty) {
-						msg.react('788158698199449612');
-					}
-				}
-			}
-
-	//#endregion
-		
-	//#region Dank Memer Unscrambler
+		//#region Dank Memer Unscrambler
 		if (/*msg.guild == this.client.guilds.cache.get('861359734941810689') || */ msg.guild == this.client.guilds.cache.get('502208815937224715') /*..*/ || msg.guild == this.client.guilds.cache.get('852562130199773244') /*The Bee Nest*/) {
 			if ((msg.author.id == '270904126974590976' /*Dank Memer*/ || msg.author.id == '319183644331606016' /*tim.*/)) {
 				if (msg.content.includes('Scramble -')) {
@@ -140,8 +60,50 @@ class MessageListener extends Listener {
 				}
 			}
 		}
-	//#endregion
+		//#endregion
+	}
+}
 
+function findWords(words, msg) {
+	const foundWords = words.find((word) => {
+		const regex = new RegExp(`\\b${word}\\b`, 'i');
+		return regex.test(msg.content);
+	});
+	
+	if (foundWords) {
+		switch (words[0]) {
+			case 'xav':
+				msg.react('717915043852845157');
+				break;
+			case 'ham':
+				//msg.react('776204703259951134'); // hamburger
+				msg.react('784122296784257075'); // dark monster thing
+				break;
+			case 'thor':
+				//msg.react('779704880792469514'); // purple
+				//msg.react('779745006944256010'); // corn
+				msg.react('782662912225443841'); // irl pfp
+				break;
+			case 'xir':
+				msg.react('764192509773479977');
+				break;
+			case 'wal':
+				//msg.react('779720093608837130'); // wal default
+				msg.react('786654445013827616'); // female wal
+				break;
+			case 'aga':
+				msg.react('779730263524900875');
+				break;
+			case 'drag':
+				msg.react('779736525533216829');
+				break;
+			case 'yonnor':
+				msg.react('781222395603451914');
+				break;
+			case 'mighty':
+				msg.react('788158698199449612');
+				break;
+		}
 	}
 }
 
