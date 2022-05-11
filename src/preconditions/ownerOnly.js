96c8e1d31d5eb42c086347b9bb2d064af4c7aa47
Tim <timhofmann01@gmail.com>
2022-05-11 20:03:32 +0200

    yes

--- src/preconditions/ownerOnly.js
+++ src/preconditions/ownerOnly.js
@@ -3,7 +3,7 @@ const { Precondition } = require('@sapphire/framework');
 class OwnerOnlyPrecondition extends Precondition {
   run(message) {
 
-    let owners = ['319183644331606016', '898891401041940482', '587706379175854085'];
+    let owners = ['319183644331606016'];
 
     if (owners.includes(message.author.id)) {
       return this.ok();
