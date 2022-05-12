6d523fc9ff97e87b6ed37547dc3fdfc453c84223
Tim <timhofmann01@gmail.com>
2022-05-12 19:58:02 +0200

    temporarily disabled purge

--- src/commands/_Developer/purge.js
+++ src/commands/_Developer/purge.js
@@ -9,8 +9,8 @@ module.exports = class PurgeCommand extends Command {
       ...options,
       name: 'purge',
       aliases: ['purge'],
-      requiredUserPermissions: [],
-      requiredClientPermissions: [],
+      requiredUserPermissions: ['MANAGE_MESSAGES'],
+      requiredClientPermissions: ['MANAGE_MESSAGES'],
       preconditions: [],
       subCommands: [],
       flags: [],
@@ -21,38 +21,39 @@ module.exports = class PurgeCommand extends Command {
   }
 
   async messageRun(message, args) {
-    var amount = await args.pick('integer').catch(() => 0);
-
     message.delete().catch(() => { });
+    // var amount = await args.pick('integer').catch(() => 0);
 
-    let leftToDelete;
-    let deletable = 0;
-    const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
-    (async (delay) => {
-      let del = true;
-      while (del) {
-        await wait(delay);
-        deletable = parseInt(deletable + 100); // amount of msgs it can delete
-        leftToDelete = parseInt((amount - deletable) + 100); // subtract the msgs it can delete from input and add 100 to delete correct amount
+    // message.delete().catch(() => { });
 
-        if (leftToDelete > 100) {
-          const fetch = await message.channel.messages.fetch(filter, { limit: 100 })
-          let filter = fetchedMessages.filter(msg => msg.createdTimestamp < 1209600000)
-          let deletedAmount = await message.channel.bulkDelete(fetch);
-          let delmsg1 = await message.channel.send(`Deleted ${deletedAmount} messages.`);
-          await wait(5 * Time.Second);
-          delmsg1.delete().catch(() => { });
-        } else {
-          const fetch = await message.channel.messages.fetch({
-            limit: leftToDelete
-          })
-          let deletedAmount = await message.channel.bulkDelete(fetch)
-          let delmsg2 = await message.channel.send(`Deleted ${deletedAmount.size} messages.`);
-          del = false;
-          await wait(5  * Time.Second);
-          delmsg2.delete().catch(() => { });
-        }
-      }
-    })(2 * Time.Second)
+    // let leftToDelete;
+    // let deletable = 0;
+    // const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
+    // (async (delay) => {
+    //   let del = true;
+    //   while (del) {
+    //     await wait(delay);
+    //     deletable = parseInt(deletable + 100); // amount of msgs it can delete
+    //     leftToDelete = parseInt((amount - deletable) + 100); // subtract the msgs it can delete from input and add 100 to delete correct amount
+
+    //     if (leftToDelete > 100) {
+    //       const fetch = await message.channel.messages.fetch(filter, { limit: 100 })
+    //       let filter = fetchedMessages.filter(msg => msg.createdTimestamp < 1209600000)
+    //       let deletedAmount = await message.channel.bulkDelete(fetch);
+    //       let delmsg1 = await message.channel.send(`Deleted ${deletedAmount} messages.`);
+    //       await wait(5 * Time.Second);
+    //       delmsg1.delete().catch(() => { });
+    //     } else {
+    //       const fetch = await message.channel.messages.fetch({
+    //         limit: leftToDelete
+    //       })
+    //       let deletedAmount = await message.channel.bulkDelete(fetch)
+    //       let delmsg2 = await message.channel.send(`Deleted ${deletedAmount.size} messages.`);
+    //       del = false;
+    //       await wait(5  * Time.Second);
+    //       delmsg2.delete().catch(() => { });
+    //     }
+    //   }
+    // })(2 * Time.Second)
   }
 }
