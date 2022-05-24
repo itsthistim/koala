c63ad7f64c71d53be340476b50bdd22deb330911
Tim <timhofmann01@gmail.com>
2022-05-24 17:59:59 +0200

    setup command

--- src/commands/_Developer/setup.js
+++ src/commands/_Developer/setup.js
@@ -1,49 +1,64 @@
-const { Command, CommandOptionsRunTypeEnum, BucketScope } = require('@sapphire/framework');
-const { send, reply } = require('@sapphire/plugin-editable-commands');
-const { MessageEmbed } = require('discord.js');
-const { Time } = require('@sapphire/time-utilities');
+const {Command, CommandOptionsRunTypeEnum, BucketScope} = require("@sapphire/framework");
+const { send, reply } = require("@sapphire/plugin-editable-commands");
+const { MessageEmbed } = require("discord.js");
+const { Time } = require("@sapphire/time-utilities");
 
 module.exports = class SyncCommand extends Command {
-    constructor(context, options) {
-        super(context, {
-            ...options,
-            name: 'sync',
-            aliases: ['sync'],
-            requiredUserPermissions: [],
-            requiredClientPermissions: [],
-            preconditions: ['ownerOnly'],
-            subCommands: [],
-            flags: [],
-            options: [],
-            nsfw: false,
-            description: {
-                content: 'Synchonizes permissions with parent channel.',
-                usage: '',
-                examples: ['']
-            }
+  constructor(context, options) {
+    super(context, {
+      ...options,
+      name: "setup",
+      aliases: ["setup", "sync"],
+      requiredUserPermissions: [],
+      requiredClientPermissions: [],
+      preconditions: ["ownerOnly"],
+      subCommands: [],
+      flags: [],
+      options: [],
+      nsfw: false,
+      description: {
+        content: "Sets up the server.",
+        usage: "[mute] [sync]",
+        examples: [""],
+      },
+    });
+  }
+
+  async messageRun(message, args) {
+    message.delete().catch(() => {});
+
+    var actions = await args.repeat("string").catch(() => null);
+
+    if (!actions) return reply(message, "No actions specified.");
+
+    var mute = actions.includes("mute") || actions.includes("muted") || actions.includes("mutes");
+    var sync = actions.includes("sync");
+
+    if (!mute && !sync) return reply(message, "No actions specified.");
+
+    if (mute) {
+      // set mute role permissions for all channels
+      let muterole = message.guild.roles.cache.find(
+        (role) => role.name === "Muted"
+      );
+
+      let channels = message.guild.channels.cache;
+      channels.forEach((channel) => {
+        channel.permissionOverwrites.create(muterole, {
+          SEND_MESSAGES: false,
+          SPEAK: false,
+          ADD_REACTIONS: false,
         });
+      });
     }
 
-    async messageRun(message, args) {
-        message.delete().catch(() => { });
-
-        // set mute role permissions for all channels
-        let muterole = message.guild.roles.cache.find(role => role.name === 'Muted');
-        let channels = message.guild.channels.cache;
-
-        channels.forEach(channel => {
-            channel.permissionOverwrites.create(muterole, {
-                SEND_MESSAGES: false,
-                SPEAK: false,
-                ADD_REACTIONS: false
-            });
-        });
-
-        // sync permissions with parent
-        channels.forEach(channel => {
-            if (channel.parent) {
-                channel.lockPermissions().catch(console.error);
-            }
-        });
+    if (sync) {
+      // sync permissions with parent
+      channels.forEach((channel) => {
+        if (channel.parent) {
+          channel.lockPermissions().catch(console.error);
+        }
+      });
     }
-}
+  }
+};
