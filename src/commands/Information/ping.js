9b1327dff7ba5cb6822d919cd21ce572dd811dfa
Tim <timhofmann01@gmail.com>
2022-06-01 19:29:13 +0200

    slash command tests

--- src/commands/Information/ping.js
+++ src/commands/Information/ping.js
@@ -1,34 +1,59 @@
-const { Command } = require('@sapphire/framework');
-const { send, reply } = require('@sapphire/plugin-editable-commands');
+const { Command } = require("@sapphire/framework");
+const { isMessageInstance } = require("@sapphire/discord.js-utilities");
+const { send, reply } = require("@sapphire/plugin-editable-commands");
 
 module.exports = class PingCommand extends Command {
   constructor(context, options) {
     super(context, {
       ...options,
-      name: 'ping',
-      aliases: ['pong'],
+      name: "ping",
+      aliases: ["pong"],
       preconditions: [],
-      description: {
-        content: 'Pong!',
-        usage: '',
-        examples: []
-      }
+      description: "See the bot's latency.",
+      chatInputCommand: {
+        register: true,
+      },
     });
   }
 
   async messageRun(message) {
     const msg = await reply(message, {
-      embeds: [{
-        description: `🤖 API latency: **${this.container.client.ws.ping} ms**`,
-        color: COLORS.RED
-      }]
+      embeds: [
+        {
+          description: `🤖 API latency: **${this.container.client.ws.ping} ms**`,
+          color: COLORS.RED,
+        },
+      ],
     });
 
     return send(message, {
-      embeds: [{
-        description: `🤖 API latency: **${this.container.client.ws.ping} ms**\n💬 Message latency: **${msg.createdTimestamp - message.createdTimestamp} ms**\n`,
-        color: COLORS.GREEN
-      }]
+      embeds: [
+        {
+          description: `🤖 API latency: **${
+            this.container.client.ws.ping
+          } ms**\n💬 Message latency: **${
+            msg.createdTimestamp - message.createdTimestamp
+          } ms**\n`,
+          color: COLORS.GREEN,
+        },
+      ],
     });
   }
-}
+
+  async chatInputRun(interaction) {
+    const ping = Math.round(this.container.client.ws.ping);
+    const msg = await interaction.reply({
+      content: `🤖 API latency: **${ping} ms**`,
+      ephemeral: true,
+      fetchReply: true,
+    });
+
+    if (isMessageInstance(msg)) {
+      const diff = msg.createdTimestamp - interaction.createdTimestamp;
+      return interaction.editReply(
+        `🤖 API latency: **${ping}ms**\n💬 Message latency: **${diff}ms**\n`
+      );
+    }
+    return interaction.editReply("Failed to retrieve ping.");
+  }
+};
