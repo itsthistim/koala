f01d4e271270c4c2a957f159173aa6d71142c591
Tim <timhofmann01@gmail.com>
2022-06-02 14:58:54 +0200

    new purge

--- src/commands/Moderation/purge.js
+++ src/commands/Moderation/purge.js
@@ -26,8 +26,13 @@ module.exports = class PurgeCommand extends Command {
 
   async messageRun(message, args) {
     var amount = await args.pick("integer").catch(() => 0);
-    
-    if(amount > 1000) { return reply(message, "You can only delete up to 1000 messages at a time."); } 
+
+    if (amount > 1000) {
+      return reply(
+        message,
+        "You can only delete up to 1000 messages at a time."
+      );
+    }
 
     amount++; // include the message that triggered the command
     var deleted = 0; // number of messages deleted
@@ -35,14 +40,27 @@ module.exports = class PurgeCommand extends Command {
 
     // keep deleting messages in 100 message chunks until a deletable amount of messages is reached
     while (amount > 100) {
-      deleted = await message.channel.bulkDelete((await message.channel.messages.fetch({limit: 100})).filter(message => !message.pinned && message.deletable), true);
-      deletedTotal += deleted;
-      amount -= deleted;
+      deleted = await message.channel.bulkDelete((await message.channel.messages.fetch({ limit: 100 })).filter((message) => !message.pinned && message.deletable), true);
+      deletedTotal += deleted.size;
+      amount -= deleted.size; // TODO: potentially have to rewrite to subtract 100 to prevent possible infinite loops
     }
 
     // delete the remaining messages
     if (amount > 0) {
-      deletedTotal += await message.channel.bulkDelete((await message.channel.messages.fetch({limit: amount})).filter(message => !message.pinned && message.deletable), true);
+      deleted = await message.channel.bulkDelete(
+        (
+          await message.channel.messages.fetch({ limit: amount })
+        ).filter((message) => !message.pinned && message.deletable),
+        true
+      );
+      deletedTotal += deleted.size;
     }
+
+    // send response which deletes itself after 5 seconds
+    deletedTotal--; // remove the message that triggered the command from count
+    let response = await send(message, `Deleted ${deletedTotal} messages.`);
+    setTimeout(() => {
+      response.delete();
+    }, 3 * Time.Second);
   }
 };
