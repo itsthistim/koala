ae0aff05d1886806faf816a5d83ae04b213c9771
Tim <timhofmann01@gmail.com>
2022-06-13 19:37:59 +0200

    added bot setup action

--- src/commands/_Developer/setup.js
+++ src/commands/_Developer/setup.js
@@ -1,4 +1,4 @@
-const {Command, CommandOptionsRunTypeEnum, BucketScope} = require("@sapphire/framework");
+const { Command, CommandOptionsRunTypeEnum, BucketScope } = require("@sapphire/framework");
 const { send, reply } = require("@sapphire/plugin-editable-commands");
 const { MessageEmbed } = require("discord.js");
 const { Time } = require("@sapphire/time-utilities");
@@ -18,41 +18,54 @@ module.exports = class SyncCommand extends Command {
       nsfw: false,
       description: {
         content: "Sets up the server.",
-        usage: "[mute] [sync]",
+        usage: "[mute] [bot] [sync]",
         examples: [""],
       },
+      detailedDescription: '\nSets up the server.\n\n**Mute**: Takes a role and sets all channels to have that role. Also denies permissions to communicate with other members. They can still join voice channels but not talk in them.\n\n**Bot**: Takes a role and attempts to assign all bots to this role.\n\n**Sync**: Syncs permissions with the parent channel.',
     });
   }
 
   async messageRun(message, args) {
-    message.delete().catch(() => {});
+    message.delete().catch(() => { });
 
     var action = await args.pick("string").catch(() => null);
-    var muterole = await args.pick("role").catch(() => null);
 
-    if (!action) return reply(message, "No action specified.");
-    if (action != "mute" && action != "sync") return reply(message, "Invalid action.");
+    let channels = message.guild.channels.cache;
 
-    if (action == "mute") {
-      if (!muterole) return reply(message, "No muterole specified.");
+    switch (action) {
+      case "mute":
+        var muterole = await args.pick("role").catch(() => null);
+        if (!muterole) return reply(message, "No muterole specified.");
 
-      let channels = message.guild.channels.cache;
-      channels.forEach((channel) => {
-        channel.permissionOverwrites.create(muterole, {
-          SEND_MESSAGES: false,
-          SPEAK: false,
-          ADD_REACTIONS: false,
+
+        channels.forEach((channel) => {
+          channel.permissionOverwrites.create(muterole, {
+            SEND_MESSAGES: false,
+            SPEAK: false,
+            ADD_REACTIONS: false
+          });
         });
-      });
-    }
+        break;
+      case "sync":
+        channels.forEach((channel) => {
+          if (channel.parent) {
+            channel.lockPermissions().catch(console.error);
+          }
+        });
+        break;
+      case "bot":
+        var botrole = await args.pick("role").catch(() => null);
+        if (!botrole) return reply(message, "No botrole specified.");
 
-    if (action == "sync") {
-      // sync permissions with parent
-      channels.forEach((channel) => {
-        if (channel.parent) {
-          channel.lockPermissions().catch(console.error);
-        }
-      });
+        message.guild.members.cache.forEach((member) => {
+          if (member.user.bot) {
+            member.roles.add(botrole).catch(() => { });
+          }
+        });
+        break;
+      default:
+        message.channel.send("Please specify a valid action.");
+        break;
     }
   }
 };
