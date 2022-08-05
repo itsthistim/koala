eb73ba5c4a9ebd7872c5aa60e4466662fc025d95
Tim <timhofmann01@gmail.com>
2022-08-06 00:41:25 +0200

    profit calc

---
+++ src/commands/Information/profit.js
@@ -0,0 +1,51 @@
+const { Command } = require('@sapphire/framework');
+const { send, reply } = require('@sapphire/plugin-editable-commands');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = class ProfitCommand extends Command {
+    constructor(context, options) {
+        super(context, {
+            ...options,
+            name: 'profit',
+            aliases: ['profit'],
+            requiredUserPermissions: [],
+            requiredClientPermissions: [],
+            preconditions: [],
+            subCommands: [],
+            flags: [],
+            options: [],
+            nsfw: false,
+            description: {
+                content: 'Calculates profit made from the sauce market.',
+                usage: '<bought for> <sells for> <amount>',
+                examples: ['']
+            },
+            detailedDescription: ''
+        });
+    }
+
+    // registerApplicationCommands(registry) {
+    //     registry.registerChatInputCommand((builder) => {
+    //         builder.setName(this.name)
+    //         builder.setDescription(this.description.content)
+    //     }, {
+    //         //idHints: ''
+    //     })
+    // }
+
+    // async chatInputRun(interaction) {
+
+    // }
+
+    async messageRun(message, args) {
+        var oldPrice = await args.pick('integer').catch(() => null);
+        var newPrice = await args.pick('integer').catch(() => null);
+        var amount = await args.pick('integer').catch(() => null);
+
+        if (!oldPrice) return message.reply('Please provide the old price.');
+        if (!newPrice) return message.reply('Please provide the new price.');
+        if (!amount || amount <= 0) return message.reply('Please provide the amount.');
+
+        return reply(message, `Bought ${amount} sauce${amount > 1 ? 's' : ''} for ${oldPrice * amount}$ and sold for ${newPrice * amount}$ for a profit of **${amount * (newPrice - oldPrice)}$**.`);
+    }
+}
