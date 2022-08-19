2e50099e499c039d25fc53241d44eced12ba9c8a
Tim <timhofmann01@gmail.com>
2022-08-19 17:36:54 +0200

    Update messageCreate.js

--- src/listeners/client/messageCreate.js
+++ src/listeners/client/messageCreate.js
@@ -12,9 +12,13 @@ module.exports = class ReadyListener extends Listener {
     
     const breadguilds = ['502208815937224715', '1005574058781462648']
 
-    // if a message was sent in one of the breadguilds, and contains the word 'bread' to lowercase, react with 🍞
-    if (breadguilds.includes(message.channel.guild.id) && message.content.toLowerCase().includes('bread')) {
-      message.react('🍞');
+    if (breadguilds.includes(message.channel.guild.id)) {
+      if (message.content.toLowerCase().includes('bread')) {
+        message.react('🍞');
+      }
+      if (message.content.toLowerCase().includes('tim')) {
+        message.react('🇼');
+      }
     }
   }
 }
