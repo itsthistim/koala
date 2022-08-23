e0c909ff0ed482a4b91e405f740b955637f5cf62
Tim <timhofmann01@gmail.com>
2022-08-23 23:06:38 +0200

    only match tim

--- src/listeners/client/messageCreate.js
+++ src/listeners/client/messageCreate.js
@@ -8,15 +8,16 @@ module.exports = class ReadyListener extends Listener {
   }
 
   async run(message) {
-    
+
     const breadguilds = ['502208815937224715', '1005574058781462648']
 
     if (breadguilds.includes(message.channel.guild.id)) {
       if (message.content.toLowerCase().includes('bread')) {
         message.react('🍞');
       }
-      if (message.content.toLowerCase().includes('tim') || message.content.toLowerCase().includes('319183644331606016')) {
-        message.react('🇼');
+      if (/\btim\b/.test(message.content) || message.content.toLowerCase().includes('319183644331606016')) {
+        // message.react('🇼');
+        message.react('🧪');
       }
     }
   }
