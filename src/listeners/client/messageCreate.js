6acae286bfec4eb0e4af8dddb9f37511d0f23c78
Tim <timhofmann01@gmail.com>
2022-08-23 23:22:02 +0200

    new msg lol

--- src/listeners/client/messageCreate.js
+++ src/listeners/client/messageCreate.js
@@ -15,8 +15,13 @@ module.exports = class ReadyListener extends Listener {
       if (message.content.toLowerCase().includes('bread')) {
         message.react('🍞');
       }
+
       if (/\btim\b/i.test(message.content) || message.content.toLowerCase().includes('319183644331606016')) {
-        message.react('🇼');
+        await message.react('🇹');
+        await message.react('🇮');
+        await message.react('🇲');
+        await message.react('<:space:1011746707853299832>');
+        await message.react('🇼');
       }
     }
   }
