ac3073863b80e18efbdbe6fdcc5b97f34b7144db
Tim <timhofmann01@gmail.com>
2023-01-31 21:14:48 +0100

    improved whois again: better relative time

--- src/commands/Information/userinfo.js
+++ src/commands/Information/userinfo.js
@@ -50,72 +50,32 @@ export class UserInfoCommand extends Command {
 	}
 
 	async getInfoEmbed(interaction, user) {
+		const status = {
+			online: '<:online:861986701580697670> ',
+			idle: '<:idle:861986831541207042>',
+			dnd: '<:dnd:861986768517070938> ',
+			streaming: '<:streaming:861986871578722364>',
+			offline: '<:offline:861986750952112189> '
+		};
+
+		const key_permissions = {
+			ADMINISTRATOR: 'Administrator',
+			MANAGE_GUILD: 'Manage Server',
+			MANAGE_ROLES: 'Manage Roles',
+			MANAGE_MESSAGES: 'Manage Messages',
+			MANAGE_CHANNELS: 'Manage Channels',
+			MANAGE_THREADS: 'Manage Threads',
+			MANAGE_WEBHOOKS: 'Manage Webhooks',
+			MANAGE_NICKNAMES: 'Manage Nicknames',
+			MANAGE_EMOJIS: 'Manage Emojis',
+			BAN_MEMBERS: 'Ban Members',
+			KICK_MEMBERS: 'Kick Members',
+			MENTION_EVERYONE: 'Mention Everyone'
+		};
+
 		let member = interaction.guild.members.cache.get(user.id);
 
-		if (member) {
-			const status = {
-				online: '<:online:861986701580697670> ',
-				idle: '<:idle:861986831541207042>',
-				dnd: '<:dnd:861986768517070938> ',
-				streaming: '<:streaming:861986871578722364>',
-				offline: '<:offline:861986750952112189> '
-			};
-
-			const key_permissions = {
-				ADMINISTRATOR: 'Administrator',
-				MANAGE_GUILD: 'Manage Server',
-				MANAGE_ROLES: 'Manage Roles',
-				MANAGE_MESSAGES: 'Manage Messages',
-				MANAGE_CHANNELS: 'Manage Channels',
-				MANAGE_THREADS: 'Manage Threads',
-				MANAGE_WEBHOOKS: 'Manage Webhooks',
-				MANAGE_NICKNAMES: 'Manage Nicknames',
-				MANAGE_EMOJIS: 'Manage Emojis',
-				BAN_MEMBERS: 'Ban Members',
-				KICK_MEMBERS: 'Kick Members',
-				MENTION_EVERYONE: 'Mention Everyone'
-			};
-
-
-			let embed = new EmbedBuilder()
-				.setColor(COLORS.DEFAULT)
-				.setAuthor({
-					name: `${member.user.tag}`,
-					iconURL: user.displayAvatarURL({ dynamic: true })
-				})
-				.setThumbnail(member.displayAvatarURL({ dynamic: true }))
-				.setDescription(`${member.presence ? status[member.presence.status] : ''}${member}`)
-				.addFields(
-					{
-						name: 'Joined Server',
-						value: `<t:${Math.floor(member.joinedAt / 1000)}:f>\n<t:${Math.floor(member.joinedAt / 1000)}:R>`,
-						inline: true
-					},
-					{
-						name: 'Joined Discord',
-						value: `<t:${Math.floor(member.user.createdAt / 1000)}:f>\n<t:${Math.floor(member.user.createdAt / 1000)}:R>`,
-						inline: true
-					},
-					{
-						name: `Roles [${member.roles.cache.size - 1}]`,
-						value: member.roles.cache.size > 1 ? member.roles.cache.map((r) => r).join(' ') : 'None',
-						inline: false
-					}
-				);
-
-			if (Object.keys(key_permissions).filter((key) => member.permissions.toArray().includes(key)).length > 0) {
-				embed.addFields({
-					name: 'Key Permissions',
-					value: Object.keys(key_permissions)
-						.filter((key) => member.permissions.toArray().includes(key))
-						.map((key) => key_permissions[key])
-						.join(', '),
-					inline: false
-				});
-			}
-
-			return embed;
-		} else {
+		if (!member) {
 			return new EmbedBuilder()
 				.setColor(COLORS.DEFAULT)
 				.setAuthor({
@@ -126,10 +86,79 @@ export class UserInfoCommand extends Command {
 				.setDescription(`${user}`)
 				.addFields({
 					name: 'Joined Discord',
-					value: `${moment(user.createdAt).format('MMM Do YYYY, hh:mm:ss a')}\n(<t:${Math.floor(user.createdAt / 1000)}:R>)`,
+					value: `${moment(user.createdAt).format('MMM Do YYYY, hh:mm:ss a')}\n(${this.durationAgo(user.createdAt)})`,
 					inline: true
 				});
 		}
+
+		let embed = new EmbedBuilder()
+			.setColor(COLORS.DEFAULT)
+			.setAuthor({
+				name: `${member.user.tag}`,
+				iconURL: user.displayAvatarURL({ dynamic: true })
+			})
+			.setThumbnail(member.displayAvatarURL({ dynamic: true }))
+			.setDescription(`${member.presence ? status[member.presence.status] : ''}${member}`)
+			.addFields(
+				{
+					name: 'Joined Server',
+					value: `${moment(member.joinedAt).format('MMM Do YYYY, hh:mm:ss a')}\n(${this.durationAgo(member.joinedAt)})`,
+					inline: true
+				},
+				{
+					name: 'Joined Discord',
+					value: `${moment(member.user.createdAt).format('MMM Do YYYY, hh:mm:ss a')}\n(${this.durationAgo(member.user.createdAt)})`,
+					inline: true
+				},
+				{
+					name: `Roles [${member.roles.cache.size - 1}]`,
+					value: member.roles.cache.size > 1 ? member.roles.cache.map((r) => r).join(' ') : 'None',
+					inline: false
+				}
+			);
+
+		if (Object.keys(key_permissions).filter((key) => member.permissions.toArray().includes(key)).length > 0) {
+			embed.addFields({
+				name: 'Key Permissions',
+				value: Object.keys(key_permissions)
+					.filter((key) => member.permissions.toArray().includes(key))
+					.map((key) => key_permissions[key])
+					.join(', '),
+				inline: false
+			});
+		}
+
+		return embed;
+	}
+
+	durationAgo(date) {
+		// If the duration is less than an hour, return the minutes
+		if (Date.now() - date < 3600000) {
+			return moment.duration(Date.now() - date).format('m [minutes ago]');
+		}
+
+		// If the duration is less than a day, return the hours
+		if (Date.now() - date < 86400000) {
+			return moment.duration(Date.now() - date).format('H [hours] and m [minutes ago]');
+		}
+
+		// If the duration is less than a week, return the days
+		if (Date.now() - date < 604800000) {
+			return moment.duration(Date.now() - date).format('D [days] and H [hours ago]');
+		}
+
+		// If the duration is less than a month, return the weeks
+		if (Date.now() - date < 2592000000) {
+			return moment.duration(Date.now() - date).format('w [weeks] and d [days ago]');
+		}
+
+		// If the duration is less than a year, return the months
+		if (Date.now() - date < 31536000000) {
+			return moment.duration(Date.now() - date).format('M [months] and w [weeks ago]');
+		}
+
+		// If the duration is any longer, return the years
+		return moment.duration(Date.now() - date).format('y [years and] M [months ago]');
 	}
 
 	isMember(interaction, user) {
