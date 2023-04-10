9d17ddfd4ce053fc598476b333d7d3f5cd79b98e
Tim <timhofmann01@gmail.com>
2023-04-10 20:31:33 +0200

    time tests

--- src/commands/_Dev/test.js
+++ src/commands/_Dev/test.js
@@ -1,13 +1,13 @@
 import { Command } from '@sapphire/framework';
-import { stripIndents } from 'common-tags';
-import { cutTo, softWrap, capitalize, sendLoadingMessage, pickRandom } from '#lib/functions';
-import { reply } from '@sapphire/plugin-editable-commands';
+import { reply } from '@skyra/editable-commands';
+import parse from 'parse-duration';
+import humanizeDuration from 'humanize-duration';
 
 export class TestCommand extends Command {
 	constructor(context, options) {
 		super(context, {
 			name: 'test',
-			description: 'Testing.',
+			description: 'Testing stuff.',
 			preconditions: ['ownerOnly']
 		});
 	}
@@ -28,7 +28,25 @@ export class TestCommand extends Command {
 		interaction.reply({ content: 'test' });
 	}
 
-	async messageRun(message) {
-		message.channel.send('test');
+	async messageRun(message, args) {
+		var time = await args.rest('string').catch(() => null);
+
+		if (!time) {
+			return reply(message, 'You must specify a time.');
+		}
+
+		var duration = parse(time, 'ms');
+
+		// if (!duration) {
+		// 	return reply(message, 'You must specify a valid time.');
+		// }
+
+		// if (duration < 0) {
+		// 	return reply(message, 'You must specify a time greater than 1 second.');
+		// }
+
+		reply(message, `${time} = ${duration}ms\n\nWhich is ${humanizeDuration(duration, { units: ['y', 'mo', 'w', 'd', 'h', 'm', 's', 'ms'], conjunction: ' and ', serialComma: false })}`);
+
+		// reply(message, `${humanizeDuration(duration, { units: ['y', 'mo', 'w', 'd', 'h', 'm', 's', 'ms'], conjunction: ' and ', serialComma: false })}`);
 	}
 }
