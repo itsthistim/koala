84f3c9d8922a038c74e3b39b408a85abe8a80a8b
Tim <timhofmann01@gmail.com>
2023-04-10 20:30:50 +0200

    volume now 100 by default

---
+++ src/listeners/music/initQueue.js
@@ -0,0 +1,14 @@
+import { Listener } from '@sapphire/framework';
+
+export default class InitQueueListener extends Listener {
+	constructor(context) {
+		super(context, {
+			event: 'initQueue',
+			emitter: 'distube'
+		});
+	}
+
+	async run(queue) {
+		queue.volume = 100;
+	}
+}
