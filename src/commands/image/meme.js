543e93d4c1a72bd11b83dbfa76ef95a13155ae35
itsthistim <timhofmann01@gmail.com>
2024-07-19 21:53:44 +0200

    fix: revert randomization of meme posts

--- src/commands/image/meme.js
+++ src/commands/image/meme.js
@@ -1,6 +1,5 @@
 import { Command } from '@sapphire/framework';
 import { reply } from '@sapphire/plugin-editable-commands';
-import { StringUtil } from '#lib/util';
 import { EmbedBuilder } from 'discord.js';
 import axios from 'axios';
 import moment from 'moment';
@@ -58,7 +57,8 @@ export class MemeCommand extends Command {
 			.filter((post) => post.data.ups > min_ups);
 
 		// TODO: improve randomization of posts by using a better algorithm
-		const post = StringUtil.pickRandom(posts)?.data;
+		const post = posts[~~(Math.random() * posts.length)]?.data;
+
 		if (!post) return await interaction.editReply({ content: 'No memes found.' });
 
 		const embed = this.buildEmbed(post.title, post.ups, post.permalink, post.created, post.author, post.url, interaction);
