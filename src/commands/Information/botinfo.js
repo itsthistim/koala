0ac2f8c110d92856363ccd4072808e10b884a3e5
itsthistim <timhofmann01@gmail.com>
2024-11-12 21:25:55 +0100

    refactor: Standardize string quotes and improve code formatting in botinfo command

--- src/commands/Information/botinfo.js
+++ src/commands/Information/botinfo.js
@@ -1,23 +1,20 @@
-import { Command, version as sappVersion } from '@sapphire/framework';
-import { EmbedBuilder, version as djsVersion } from 'discord.js';
-import { reply } from '@sapphire/plugin-editable-commands';
-import moment from 'moment';
+import { Command, container, version as sappVersion } from "@sapphire/framework";
+import { EmbedBuilder, version as djsVersion } from "discord.js";
+import { reply } from "@sapphire/plugin-editable-commands";
+import moment from "moment";
 
 export class BotInfoCommand extends Command {
 	constructor(context, options) {
 		super(context, {
-			name: 'botinfo',
-			aliases: ['about', 'stats', 'ping', 'uptime', 'status'],
+			name: "botinfo",
+			aliases: ["about", "stats", "ping", "uptime", "status"],
 			requiredUserPermissions: [],
 			requiredClientPermissions: [],
 			preconditions: [],
 			flags: [],
 			options: [],
 			nsfw: false,
-			description: "Shows information about the bot, including the bot's uptime, ping, and more.",
-			detailedDescription: '',
-			usage: '',
-			examples: ['']
+			description: "Shows information about the bot, including the bot's uptime, ping, and more."
 		});
 	}
 
@@ -29,39 +26,39 @@ export class BotInfoCommand extends Command {
 		});
 	}
 
-	async chatInputRun(interaction) {
+	async chatInputRun(interaction, ctx) {
 		const embed = await this.createInfoEmbed(interaction);
 		return interaction.reply({ embeds: [embed] });
 	}
 
-	async messageRun(message, args) {
+	async messageRun(message, args, ctx) {
 		const embed = await this.createInfoEmbed(message);
 		return reply(message, { embeds: [embed] });
 	}
 
 	async createInfoEmbed(interaction) {
-		const dev = this.container.client.users.cache.get('319183644331606016');
-		const uptime = moment.duration(this.container.client.uptime).format('d[d], h[h], m[m], s[s]');
+		const dev = this.container.client.users.cache.get("319183644331606016");
+		const uptime = moment.duration(this.container.client.uptime).format("d[d], h[h], m[m], s[s]");
 
 		return new EmbedBuilder()
-			.setColor(COLORS.DEFAULT)
+			.setColor(container.colors.default)
 			.setAuthor({
 				name: this.container.client.user.username,
 				iconURL: this.container.client.user.displayAvatarURL({ dynamic: true })
 			})
 			.addFields(
 				{
-					name: 'Technical',
+					name: "Technical",
 					value: `**Uptime:** ${uptime}\n` + `**Ping:** ${this.container.client.ws.ping}ms\n` + `**Roundtrip:** ${Date.now() - interaction.createdTimestamp}ms`,
 					inline: true
 				},
 				{
-					name: 'Versions',
+					name: "Versions",
 					value: `**Node.js:** ${process.version}\n**Discord.js:** ${djsVersion}\n**Sapphire:** ${sappVersion}`,
 					inline: true
 				},
 				{
-					name: 'Discord',
+					name: "Discord",
 					value: `**Guilds:** ${this.container.client.guilds.cache.size}\n**Channels:** ${this.container.client.channels.cache.size}\n**Users:** ${this.container.client.users.cache.size}`,
 					inline: true
 				}
