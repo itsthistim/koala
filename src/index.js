21aca019832ee35a45c871f4a23c210f44dbca0b
itsthistim <timhofmann01@gmail.com>
2024-11-12 21:16:06 +0100

    refactor: Simplify client initialization Remove Distube

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import "#lib/setup";
-import { SapphireClient, ApplicationCommandRegistries, BucketScope } from "@sapphire/framework";
+import { SapphireClient, BucketScope } from "@sapphire/framework";
 import { GatewayIntentBits, Partials } from "discord.js";
 import { Time } from "@sapphire/time-utilities";
-
-import { DisTube } from "distube";
-import { YouTubePlugin } from "@distube/youtube";
-
 import fs from "fs";
+// import { DisTube } from "distube";
+// import { YouTubePlugin } from "@distube/youtube";
 
 let prefixes = [];
-
 if (process.env.NODE_ENV === "PRODUCTION") {
 	prefixes.push("-");
 } else {
@@ -20,54 +17,28 @@ const client = new SapphireClient({
 	defaultPrefix: prefixes,
 	caseInsensitivePrefixes: true,
 	caseInsensitiveCommands: true,
-	intents: [
-		GatewayIntentBits.Guilds,
-		GatewayIntentBits.GuildMembers,
-		GatewayIntentBits.GuildMessages,
-		GatewayIntentBits.GuildMessageTyping,
-		GatewayIntentBits.GuildModeration,
-		GatewayIntentBits.GuildEmojisAndStickers,
-		GatewayIntentBits.GuildIntegrations,
-		GatewayIntentBits.GuildWebhooks,
-		GatewayIntentBits.GuildInvites,
-		GatewayIntentBits.GuildVoiceStates,
-		GatewayIntentBits.GuildPresences,
-		GatewayIntentBits.GuildScheduledEvents,
-		GatewayIntentBits.GuildMessageReactions,
-		GatewayIntentBits.DirectMessages,
-		GatewayIntentBits.DirectMessageReactions,
-		GatewayIntentBits.DirectMessageTyping,
-		GatewayIntentBits.MessageContent,
-		GatewayIntentBits.AutoModerationConfiguration,
-		GatewayIntentBits.AutoModerationExecution
-	],
-	defaultCooldown: {
-		delay: Time.Second * 5,
-		limit: 3,
-		filteredUsers: process.env.OWNERS.split(","),
-		scope: BucketScope.User
-	},
+	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageTyping, GatewayIntentBits.GuildModeration, GatewayIntentBits.GuildEmojisAndStickers, GatewayIntentBits.GuildIntegrations, GatewayIntentBits.GuildWebhooks, GatewayIntentBits.GuildInvites, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildPresences, GatewayIntentBits.GuildScheduledEvents, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.DirectMessages, GatewayIntentBits.DirectMessageReactions, GatewayIntentBits.DirectMessageTyping, GatewayIntentBits.MessageContent, GatewayIntentBits.AutoModerationConfiguration, GatewayIntentBits.AutoModerationExecution],
 	allowedMentions: { repliedUser: true },
 	loadMessageCommandListeners: true,
 	partials: [Partials.Channel],
 	shards: "auto"
 });
 
-client.distube = new DisTube(client, {
-	emitNewSongOnly: true,
-	leaveOnStop: false,
-	leaveOnFinish: false,
-	leaveOnEmpty: true,
-	emptyCooldown: 3,
-	savePreviousSongs: true,
-	searchSongs: 0,
-	searchCooldown: 60,
-	nsfw: false,
-	emitAddSongWhenCreatingQueue: false,
-	emitAddListWhenCreatingQueue: false,
-	youtubeCookie: process.env.YT_COOKIE,
-	plugins: [new YouTubePlugin({ cookies: JSON.parse(fs.readFileSync("../cookies.json")) })]
-});
+// client.distube = new DisTube(client, {
+// 	emitNewSongOnly: true,
+// 	leaveOnStop: false,
+// 	leaveOnFinish: false,
+// 	leaveOnEmpty: true,
+// 	emptyCooldown: 3,
+// 	savePreviousSongs: true,
+// 	searchSongs: 0,
+// 	searchCooldown: 60,
+// 	nsfw: false,
+// 	emitAddSongWhenCreatingQueue: false,
+// 	emitAddListWhenCreatingQueue: false,
+// 	youtubeCookie: process.env.YT_COOKIE,
+// 	plugins: [new YouTubePlugin({ cookies: JSON.parse(fs.readFileSync("../cookies.json")) })]
+// });
 
 const main = async () => {
 	try {
@@ -75,9 +46,11 @@ const main = async () => {
 		await client.login();
 	} catch (error) {
 		console.error(error);
-		client.destroy();
+		await client.destroy();
 		process.exit(1);
 	}
 };
 
-main();
+main().then(() => {
+	client.logger.info("Logged in!");
+});
