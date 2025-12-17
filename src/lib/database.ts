c552b067dd1d743c7f4bcbdc489ae1f877290726
itsthistim <timhofmann01@gmail.com>
2025-12-17 17:34:04 +0100

    chore: cleanup

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -6,12 +6,12 @@ if (!process.env.PG_URL) {
 } else {
 	try {
 		const url = new URL(process.env.PG_URL);
-		console.log(`[Database] Connection string protocol: '${url.protocol}'`);
+		console.info(`[Database] Connection string protocol: '${url.protocol}'`);
 	} catch (error) {
 		console.error('[Database] Failed to parse PG_URL. Is it a valid URL?');
 	}
 }
 
-export const pool = new Pool({
+export const db = new Pool({
 	connectionString: process.env.PG_URL
 });
