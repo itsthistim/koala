b14492b9df6f2099a324e15b303b7ecf495d560d
itsthistim <timhofmann01@gmail.com>
2026-01-30 18:17:04 +0100

    feat(setup): better env handling

--- src/lib/setup.ts
+++ src/lib/setup.ts
@@ -1,19 +1,27 @@
 import dotenv from 'dotenv';
-dotenv.config();
+
+if (process.argv.includes('--production')) {
+	process.env.NODE_ENV = 'PRODUCTION';
+}
+
+process.env.NODE_ENV ??= 'DEVELOPMENT';
+
+dotenv.config({
+	path: process.env.NODE_ENV === 'DEVELOPMENT' ? '.env.dev' : '.env.prod'
+});
 
 import moment from 'moment';
 import momentDurationFormat from 'moment-duration-format';
 
-momentDurationFormat(moment);
-
-process.env.NODE_ENV ??= 'development';
-
 import { ApplicationCommandRegistries, RegisterBehavior } from '@sapphire/framework';
 import '@sapphire/plugin-editable-commands/register';
-import '@sapphire/plugin-subcommands/register';
 import '@sapphire/plugin-scheduled-tasks/register';
+import '@sapphire/plugin-subcommands/register';
 import { inspect } from 'util';
 
+console.info(`[INFO] Starting bot in ${process.env.NODE_ENV} mode...`);
+
+momentDurationFormat(moment);
 // Set default behavior to bulk overwrite
 ApplicationCommandRegistries.setDefaultBehaviorWhenNotIdentical(RegisterBehavior.BulkOverwrite);
 
